d7d49a50fa650bab9c8e92048fbf091759d4f417
jasonli <chlee64@gmail.com>
2018-01-12 14:37:50 +0800

    add more text.

--- hello.py
+++ hello.py
@@ -1,1 +1,5 @@
-print('hello python & git rock')
+# This program says hello and asks for my name.
+print('hello world!')
+print('What is your name?')
+myName = input()
+print('It is good to meet you, ' + myName)
